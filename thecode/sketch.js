d7faf48d0cf2ccab7eed528e6299ea9493a7ffa4
wle4 <114623158+wle4@users.noreply.github.com>
2022-12-07 00:44:33 -0600

    Update sketch.js

--- thecode/sketch.js
+++ thecode/sketch.js
@@ -120,7 +120,9 @@ var createScene = function () {
 
     back9.material = hexMat('#FFFFFF')
 
-    var meshyy = new meshModel('sportsground.glb',2,-10,20,-10);
+    var snowman = new meshModel('snowman.glb',2,2,10,10);
+    var ground = new meshModel('snowfloor.glb',-10,2,50,50);
+    var house = new meshModel('snowhome.glb', 20, 20, 10, 10);
 
     return scene;
 };
