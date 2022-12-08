5357a610edb8c35394429f191bda074662306dfb
wle4 <114623158+wle4@users.noreply.github.com>
2022-12-08 00:51:51 -0600

    Update sketch.js

--- thecode/sketch.js
+++ thecode/sketch.js
@@ -35,87 +35,87 @@ var createScene = function () {
     scene.clearColor = new BABYLON.Color3.FromHexString('#000');
     
     //create box with params x, y, z, width, height, ddepth
-    var back1 = createBox(0,0,20.1,40,40,.01)
+    var back1 = createBox(40,10,20.1,40,40,.01)
 
     back1.material = hexMat('#FFFFFF')
 
-    var b1 = createBox(0, 0, 20, 40, 40, .01);
+    var b1 = createBox(40, 10, 20, 40, 40, .01);
 
     //BLUE.... wrap box in material colored with hex code
     b1.material = fileMat('caveman..jpg');
 
-    var b2 = createBox(40, 0, -50, 40, 40, .01);
+    var b2 = createBox(80, 10, -50, 40, 40, .01);
 
     //GREEN...........wrap box in material from local file
     b2.material = fileMat('tutt.jpg');
     b2.rotation.z += Math.PI;
 
-    var back2 = createBox(40,0,-50.1,40,40,.01)
+    var back2 = createBox(80,10,-50.1,40,40,.01)
 
     back2.material = hexMat('#FFFFFF')
     
-    var b3 = createBox(80, 0, 20, 40, 40, .01);
+    var b3 = createBox(120, 10, 20, 40, 40, .01);
 
     //wrap box in material from local file
     b3.material = fileMat('kingdomm.jpg', scene);
 
-    var back3 = createBox(80,0,20.1,40,40,.01)
+    var back3 = createBox(120,10,20.1,40,40,.01)
 
     back3.material = hexMat('#FFFFFF')
 
-    var b4 = createBox(120, 0, -50, 40, 40, .01);
+    var b4 = createBox(160, 10, -50, 40, 40, .01);
 
     //wrap box in material from local file
     b4.material = fileMat('renaissancee.jpg', scene);
     b4.rotation.z += Math.PI;
 
-    var back4 = createBox(120,0,-50.1,40,40,.01)
+    var back4 = createBox(160,10,-50.1,40,40,.01)
 
     back4.material = hexMat('#FFFFFF')
 
-    var b5 = createBox(160, 0, 20, 40, 40, .01);
+    var b5 = createBox(200, 10, 20, 40, 40, .01);
 
     //wrap box in material from local file
     b5.material = fileMat('lillyy.jpg', scene);
 
-    var back5 = createBox(160,0,20.1,40,40,.01)
+    var back5 = createBox(200,10,20.1,40,40,.01)
 
     back5.material = hexMat('#FFFFFF')
 
-    var b6 = createBox(200, 0, -50, 40, 40, .01);
+    var b6 = createBox(240, 10, -50, 40, 40, .01);
 
     //wrap box in material from local file
     b6.material = fileMat('chicagoo.jpg', scene);
 
-    var back6 = createBox(200,0,-50.1,40,40,.01)
+    var back6 = createBox(240,10,-50.1,40,40,.01)
 
     back6.material = hexMat('#FFFFFF')
     b6.rotation.z += Math.PI;
 
-    var b7 = createBox(240, 0, 20, 40, 40, .01);
+    var b7 = createBox(280, 10, 20, 40, 40, .01);
 
     //wrap box in material from local file
     b7.material = fileMat('warholl.jpg', scene);
 
-    var back7 = createBox(240,0,20.1,40,40,.01)
+    var back7 = createBox(280,10,20.1,40,40,.01)
 
     back7.material = hexMat('#FFFFFF')
 
-    var b8 = createBox(280, 0, -50, 40, 40, .01);
+    var b8 = createBox(320, 10, -50, 40, 40, .01);
 
     //wrap box in material from local file
     b8.material = fileMat('lichenn.jpg', scene);
     b8.rotation.z += Math.PI;
 
-    var back8 = createBox(280,0,-50.1,40,40,.01)
+    var back8 = createBox(320,10,-50.1,40,40,.01)
 
     back8.material = hexMat('#FFFFFF')
 
-    var back9 = createBox(100,-20.3,-10,500,0,200)
+    var back9 = createBox(100,-20.3,0,800,0,200)
 
     back9.material = hexMat('#FFFFFF')
 
-    var snowman = new meshModel('snowman.glb', 2,-5,0,1);
+    var snowman = new meshModel('snowman.glb', 2,-1,0,0);
     //var house = new meshModel('snowhome.glb', -20, 0, 20, 20);
 
     return scene;
